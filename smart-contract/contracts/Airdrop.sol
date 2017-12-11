3c954dbc1235be4f2611549920709255ca2bab9e
Yevhenii Babichenko <eugene.babichenko@gmail.com>
2017-12-11 19:57:13 +0100

    Use SafeMath

--- smart-contract/contracts/Airdrop.sol
+++ smart-contract/contracts/Airdrop.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.4.18;
 
 import 'zeppelin-solidity/contracts/token/ERC20.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract Airdrop {
+    using SafeMath for uint256;
+
     uint256 constant INITIAL_ETH_PRICE_USD = 470;
     uint256 constant MAXIMUM_ETH_PRICE_USD = 2000;
     address whitelistSupplier;
@@ -44,13 +47,13 @@ contract Airdrop {
         uint256 initialAmount = token.balanceOf(_to);
         uint256 priceDifference = 0;
         if (_actualPrice > INITIAL_ETH_PRICE_USD && _actualPrice < MAXIMUM_ETH_PRICE_USD) {
-            priceDifference = _actualPrice - INITIAL_ETH_PRICE_USD;
+            priceDifference = _actualPrice.sub(INITIAL_ETH_PRICE_USD);
         } else if (_actualPrice >= MAXIMUM_ETH_PRICE_USD) {
-            priceDifference = MAXIMUM_ETH_PRICE_USD - INITIAL_ETH_PRICE_USD;
+            priceDifference = MAXIMUM_ETH_PRICE_USD.sub(INITIAL_ETH_PRICE_USD);
         } else {
             priceDifference = 0;
         }
-        uint256 increase = initialAmount * priceDifference / INITIAL_ETH_PRICE_USD;
+        uint256 increase = initialAmount.mul(priceDifference).div(INITIAL_ETH_PRICE_USD);
         performed[_to] = true;
         token.transfer(_to, increase);
         AirdropPerformed(_to, initialAmount, increase + initialAmount, _actualPrice);
