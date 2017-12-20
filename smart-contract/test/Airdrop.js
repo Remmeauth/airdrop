1a025b6aad29a46ff57f5acf6245bb144f685972
Yevhenii Babichenko <eugene.babichenko@gmail.com>
2017-12-20 21:39:31 +0100

    clarify tests

--- smart-contract/test/Airdrop.js
+++ smart-contract/test/Airdrop.js
@@ -15,40 +15,50 @@ contract('Airdrop', (accounts) => {
         await token.transfer(airdrop.address, 10000, { from: accounts[0] });
     });
 
-    it('test airdrop correctness without increase', async () => {
-        await airdrop.performAirdrop(accounts[1], 470, { from: accounts[0] });
+    it('test airdrop correctness without increase (lower than current price)', async () => {
+        await airdrop.performAirdrop(accounts[1], 400, { from: accounts[0] });
         assert.equal(100, await token.balanceOf.call(accounts[1]));
     });
 
+    it('test airdrop correctness without increase (equal to current price)', async () => {
+        await airdrop.performAirdrop(accounts[2], 470, { from: accounts[0] });
+        assert.equal(100, await token.balanceOf.call(accounts[2]));
+    });
+
     it('test airdrop correctness wit 1.5 times rate increase)', async () => {
-        await airdrop.performAirdrop(accounts[2], 705, { from: accounts[0] });
-        assert.equal(150, await token.balanceOf.call(accounts[2]));
+        await airdrop.performAirdrop(accounts[3], 705, { from: accounts[0] });
+        assert.equal(150, await token.balanceOf.call(accounts[3]));
     });
 
     it('test airdrop correctness with 2 times rate increase)', async () => {
-        await airdrop.performAirdrop(accounts[3], 940, { from: accounts[0] });
-        assert.equal(200, await token.balanceOf.call(accounts[3]));
+        await airdrop.performAirdrop(accounts[4], 940, { from: accounts[0] });
+        assert.equal(200, await token.balanceOf.call(accounts[4]));
+    });
+
+    it('test airdrop correctness with increase equal to maximum', async () => {
+        await airdrop.performAirdrop(accounts[5], 2000, { from: accounts[0] }); 
+        assert.equal(425, await token.balanceOf.call(accounts[5]));
+    });
+
+    it('test airdrop correctness with increase more than maximum', async () => {
+        await airdrop.performAirdrop(accounts[6], 2100, { from: accounts[0] }); 
+        assert.equal(425, await token.balanceOf.call(accounts[6]));
     });
 
     it('ensure that airdrop cannot be performed twice', async () => {
-        await airdrop.performAirdrop(accounts[4], 940, { from: accounts[0] });
-        await expectThrow(airdrop.performAirdrop(accounts[4], 940, { from: accounts[0] }));
+        await airdrop.performAirdrop(accounts[7], 940, { from: accounts[0] });
+        await expectThrow(airdrop.performAirdrop(accounts[7], 940, { from: accounts[0] }));
     });
 
     it('ensure that only authorized account can perform airdrop', async () => {
-        await expectThrow(airdrop.performAirdrop(accounts[5], 940, { from: accounts[1] }));
-    });
-
-    it('test airdrop correctness with dropped rate', async () => {
-        await airdrop.performAirdrop(accounts[6], 200, { from: accounts[0] });
-        assert.equal(100, await token.balanceOf.call(accounts[6]));
+        await expectThrow(airdrop.performAirdrop(accounts[8], 940, { from: accounts[1] }));
     });
 
     it('only authorized account can finalize', async () => {
         await expectThrow(airdrop.finalize({ from: accounts[0] }));
     });
 
-    it('ensure that money are sent', async () => {
+    it('ensure that money are sent after finalization', async () => {
         let initialAmount = await token.balanceOf(accounts[1]);
         let contractAmount = await token.balanceOf(airdrop.address);
         await airdrop.finalize({ from: accounts[1] });
@@ -62,11 +72,6 @@ contract('Airdrop', (accounts) => {
 
     it('cannot send funds after finalization', async () => {
         await airdrop.finalize({ from: accounts[1] });
-        await expectThrow(airdrop.performAirdrop(accounts[7], 200, { from: accounts[0] }));
-    });
-
-    it('try rate more than maximum', async () => {
-        await airdrop.performAirdrop(accounts[8], 2100, { from: accounts[0] }); 
-        assert.equal(425, await token.balanceOf.call(accounts[8]));
+        await expectThrow(airdrop.performAirdrop(accounts[9], 200, { from: accounts[0] }));
     });
 });
