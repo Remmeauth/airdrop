71a9b6bbdda351de74f595f8ed391e36f46da75e
Yevhenii Babichenko <eugene.babichenko@gmail.com>
2017-12-20 21:42:18 +0100

    migrate to the latest version of web3

--- whitelist-uploader/server.js
+++ whitelist-uploader/server.js
@@ -4,27 +4,23 @@ let fs = require('fs');
 
 let account = require("./accountKey.json");
 
-let abiFile = fs.readFileSync("abi.json");
+let abiFile = fs.readFileSync("Airdrop.json");
 let abi = JSON.parse(abiFile);
 const actualRate = 500;
 const contractAddress = "0x2342423423";
-const callerAddress = "0x2342423423";
-const callerPass = "********";
 
 let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-let contract = web3.eth.contract(abi).at(contractAddress);
+let contract = new web3.eth.Contract(abi, contractAddress);
 
 firebase.initializeApp({
     credential: firebase.credential.cert(account),
     databaseURL: "https://remme-ico.firebaseio.com"
 });
 
-web3.personal.unlockAccount(callerAddress, callerPass, 1000);
-
 firebase.firestore().collection('users').get().then((snapshot) => {
     snapshot.forEach((doc) => {
         let hash = doc.data().hash;
         console.log(hash);
-        console.log(await contract.performAirdrop(hash, actualRate));
+        console.log(await contract.methods.performAirdrop(hash, actualRate).send());
     });
-})
+});
