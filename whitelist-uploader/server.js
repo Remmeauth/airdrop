b676e64e0122ecdc0f6bad0d3fee56ed175cb37b
Yevhenii Babichenko <eugene.babichenko@gmail.com>
2017-12-25 00:57:07 +0100

    server.js: load files in a smart way & fix code style

--- whitelist-uploader/server.js
+++ whitelist-uploader/server.js
@@ -1,26 +1,22 @@
 let firebase = require('firebase-admin');
 let Web3 = require('web3');
-let fs = require('fs');
 
-let account = require("./accountKey.json");
+const account = require('./accountKey.json');
+const abi = require('./Airdrop.json');
+const settings = require('./settings.json');
 
-let abiFile = fs.readFileSync("Airdrop.json");
-let abi = JSON.parse(abiFile);
-const actualRate = 500;
-const contractAddress = "0x2342423423";
-
-let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-let contract = new web3.eth.Contract(abi, contractAddress);
+let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+let contract = new web3.eth.Contract(abi, settings.contractAddress);
 
 firebase.initializeApp({
     credential: firebase.credential.cert(account),
-    databaseURL: "https://remme-ico.firebaseio.com"
+    databaseURL: 'https://remme-ico.firebaseio.com'
 });
 
 firebase.firestore().collection('users').get().then((snapshot) => {
     snapshot.forEach((doc) => {
         let hash = doc.data().hash;
         console.log(hash);
-        console.log(await contract.methods.performAirdrop(hash, actualRate).send());
+        console.log(await contract.methods.performAirdrop(hash, settings.actualRate).send());
     });
 });
